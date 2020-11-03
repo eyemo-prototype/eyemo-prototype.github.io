a0b319117f45979781a4e3945b61f1e73d5eec6f
Anton Ulyanov <anton.ulianov@gmail.com>
2020-11-03 20:24:33 +0700

    fix minutes display

--- src/utils/format-time.ts
+++ src/utils/format-time.ts
@@ -6,7 +6,7 @@ function pad(x: number) {
 export function formatTime(duration: number | null, long = false) {
 	if (!duration) return `00:00:00.0`
 	const hours = Math.floor(duration / 60 / 60)
-	const mins = Math.floor(duration / 60)
+	const mins = Math.floor((duration / 60) % 60)
 	const seconds = Math.floor(duration % 60)
 	const fract = Math.floor((duration - Math.floor(duration)) * 10)
 	let time = `${pad(hours)}:${pad(mins)}:${pad(seconds)}`
