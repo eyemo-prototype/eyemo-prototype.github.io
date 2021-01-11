88935a71402faeb78a89c5d3f261b0a42c9f8b78
Ksenia Platova <kplatova@gmail.com>
2021-01-11 19:58:59 +0000

    Вынести логотип из App (в дальнейшем это будет шапка)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './App.css'
 import PlayerPanel from './components/PlayerPanel'
 import CutsPanel from './components/CutsPanel'
+import Logo from './components/Logo'
 import styles from './App.module.css'
 import { Container, createMuiTheme, Grid, ThemeProvider } from '@material-ui/core'
 import DateFnsAdapter from '@material-ui/pickers/adapter/date-fns'
@@ -25,6 +26,9 @@ function App() {
 		<ThemeProvider theme={theme}>
 			<LocalizationProvider dateAdapter={DateFnsAdapter}>
 				<Container className={styles.container}>
+					<Grid className={styles.header} container alignContent='center'>
+						<Logo />
+					</Grid>
 					<Grid container wrap={'nowrap'}>
 						<Grid item lg className={'padded'}>
 							<PlayerPanel />
